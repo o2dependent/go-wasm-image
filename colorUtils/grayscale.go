3dbc7b762754b0712edf3ca628785dc401f0407c
Ethan Olsen <131eolsen@gmail.com>
2025-02-08 00:35:00 -0800

    feat: color range passed from the frontend

--- colorUtils/grayscale.go
+++ colorUtils/grayscale.go
@@ -1,7 +1,7 @@
 package colorUtils
 
-func RGBtoGrayscale(r, g, b uint8) uint8 {
+func RGBtoGrayscale(r, g, b uint8) float64 {
 	// Standard grayscale conversion using luminance formula
-	gray := uint8(0.299*float64(r) + 0.587*float64(g) + 0.114*float64(b))
+	gray := float64(0.299*float64(r) + 0.587*float64(g) + 0.114*float64(b))
 	return gray
 }
--- main.go
+++ main.go
@@ -12,20 +12,48 @@ func processImage(this js.Value, args []js.Value) interface{} {
 	uint8Array := args[0]
 	width := args[1].Int()
 	height := args[2].Int()
+	colorRangeJS := args[3]
 
 	// Convert the Uint8ClampedArray to a Go slice
 	length := uint8Array.Length()
 	imageData := make([]uint8, length)
 	js.CopyBytesToGo(imageData, uint8Array)
 
-	// Handle pixels
-	colorRange := [][]int{
-		{155, 86, 116},
-		{203, 179, 255},
-		{216, 191, 216},
-		{199, 251, 150},
-		{174, 255, 247},
+	colorRangeLength := colorRangeJS.Length()
+	colorRangeFull := make([]uint8, colorRangeLength)
+	js.CopyBytesToGo(colorRangeFull, colorRangeJS)
+
+	colorRange := [][]uint8{}
+	for i := 0; i < len(colorRangeFull)/3; i++ {
+		colors := []uint8{}
+		for j := 0; j < 3; j++ {
+			colors = append(colors, colorRangeFull[(i*3)+j])
+		}
+		colorRange = append(colorRange, colors)
 	}
+
+	// Handle pixels
+	// colorRange := [][]int{
+	// 	{155, 86, 116},
+	// 	{203, 179, 255},
+	// 	{216, 191, 216},
+	// 	{174, 255, 247},
+	// 	{199, 251, 150},
+	// }
+	// colorRange := [][]int{
+	// 	{5, 31, 57},
+	// 	{74, 36, 128},
+	// 	{197, 58, 157},
+	// 	{255, 142, 128},
+	// 	{255, 255, 255},
+	// }
+	// colorRange := [][]int{
+	// 	{13, 16, 27},
+	// 	{40, 26, 45},
+	// 	{107, 35, 65},
+	// 	{175, 39, 71},
+	// 	{238, 36, 61},
+	// }
 	imageData = ditherImage(imageData, colorRange, width, height)
 
 	array := js.Global().Get("Uint8ClampedArray").New(length)
@@ -45,8 +73,8 @@ func main() {
 	select {}
 }
 
-func ditherImage(imageData []uint8, colorRange [][]int, width int, height int) []uint8 {
-	bayerMatrix4x4 := [][]int{
+func ditherImage(imageData []uint8, colorRange [][]uint8, width int, height int) []uint8 {
+	bayerMatrix4x4 := [][]uint8{
 		{0, 8, 2, 10},
 		{12, 4, 14, 6},
 		{3, 11, 1, 9},
@@ -66,16 +94,16 @@ func ditherImage(imageData []uint8, colorRange [][]int, width int, height int) [
 			// a := imageData[imgDataIdx+3]
 			grayValue := colorUtils.RGBtoGrayscale(r, g, b)
 
-			bayerValue := bayerMatrix4x4[y%bayerN][x%bayerN] / (bayerN ^ 2)
-			outputColor := (grayValue) + (uint8(bayerR) * uint8(bayerValue))
+			bayerValue := bayerMatrix4x4[x%bayerN][y%bayerN] / uint8(bayerN^2)
+			outputColor := (grayValue) + (float64(bayerR) * float64(bayerValue))
 			outputIndex := min(
 				len(colorRange)-1,
-				int(math.Floor(float64(outputColor)/255*(float64(len(colorRange)-1)))),
+				int(math.Floor(outputColor/255*(float64(len(colorRange)-1)))),
 			)
 			colorResult = colorRange[outputIndex]
-			imageData[imgDataIdx] = uint8(colorResult[0])
-			imageData[imgDataIdx+1] = uint8(colorResult[1])
-			imageData[imgDataIdx+2] = uint8(colorResult[2])
+			imageData[imgDataIdx] = colorResult[0]
+			imageData[imgDataIdx+1] = colorResult[1]
+			imageData[imgDataIdx+2] = colorResult[2]
 			// imageData[imgDataIdx+3] = 0
 		}
 	}
