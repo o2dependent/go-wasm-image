ddaeb3ce2b3e66113c6afb58e53b43835d6f2045
Ethan Olsen <131eolsen@gmail.com>
2025-02-10 12:33:41 -0800

    feat: process area scroll

--- frontend/src/components/ProcessArea/ProcessArea.tsx
+++ frontend/src/components/ProcessArea/ProcessArea.tsx
@@ -6,7 +6,7 @@ import {
 	type GraphicsProcess,
 } from "../../stores/imageProcess";
 import { AddProcessButton } from "./AddProcessButton";
-import { Card, Flex, Grid, Heading } from "@radix-ui/themes";
+import { Box, Card, Flex, Grid, Heading, ScrollArea } from "@radix-ui/themes";
 import { DitherControls } from "./ProcessControls/DitherControls";
 import { MaskControls } from "./ProcessControls/MaskControls";
 import { SortPixelsControls } from "./ProcessControls/SortPixelsControls";
@@ -16,32 +16,39 @@ export const ProcessArea = () => {
 	const graphicProcesses = useStore($graphicProcesses);
 
 	return (
-		<Flex width={{ initial: "100%", md: "600px" }} gap="2" direction="column">
-			<Heading size="6">Graphics Processes</Heading>
-			<ProcessButton />
-			<Card>
-				<Heading size="3" mb="2">
-					Add Process
-				</Heading>
-				<Grid gap="1" columns="2">
-					{graphicProcessTypes?.map((type) => (
-						<AddProcessButton key={type} type={type} />
-					))}
-				</Grid>
-			</Card>
-			<Flex gap="1" direction="column">
-				{Object.keys(graphicProcesses)?.map((id) => {
-					const gp = graphicProcesses[id];
-					if (gp.type === "dither") {
-						return <DitherControls id={id} process={gp} key={id} />;
-					} else if (gp.type === "mask") {
-						return <MaskControls id={id} process={gp} key={id} />;
-					} else if (gp.type === "sortPixels") {
-						return <SortPixelsControls id={id} process={gp} key={id} />;
-					}
-					return null;
-				})}
+		<Box width={{ initial: "100%", md: "600px" }}>
+			<Flex gap="2" direction="column">
+				<Heading size="6">Graphics Processes</Heading>
+				<ProcessButton />
+				<ScrollArea
+					scrollbars="vertical"
+					style={{ height: 500, borderRadius: "var(--radius-2)" }}
+				>
+					<Card mb="1">
+						<Heading size="3" mb="2">
+							Add Process
+						</Heading>
+						<Grid gap="1" columns="2">
+							{graphicProcessTypes?.map((type) => (
+								<AddProcessButton key={type} type={type} />
+							))}
+						</Grid>
+					</Card>
+					<Flex gap="1" direction="column">
+						{Object.keys(graphicProcesses)?.map((id) => {
+							const gp = graphicProcesses[id];
+							if (gp.type === "dither") {
+								return <DitherControls id={id} process={gp} key={id} />;
+							} else if (gp.type === "mask") {
+								return <MaskControls id={id} process={gp} key={id} />;
+							} else if (gp.type === "sortPixels") {
+								return <SortPixelsControls id={id} process={gp} key={id} />;
+							}
+							return null;
+						})}
+					</Flex>
+				</ScrollArea>
 			</Flex>
-		</Flex>
+		</Box>
 	);
 };
